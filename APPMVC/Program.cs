80f595dba9916db1646b5ad2f28e42dbbd08965a
KimHoangLong <iamlong272004@gmail.com>
2024-10-05 09:58:54 +0700

    Danh Mục, Chất Liệu, Thương Hiệu

--- APPMVC/Program.cs
+++ APPMVC/Program.cs
@@ -5,13 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
-builder.Services.AddTransient<IServicegiaygiay, Servicegiaygiay>();
 
-builder.Services.AddHttpClient();
+
+builder.Services.AddTransient<IServicegiaygiay, Servicegiaygiay>();
 builder.Services.AddTransient<IDanhMucService, DanhMucService>();
 builder.Services.AddTransient<IThuongHieuService, ThuongHieuService>();
 builder.Services.AddTransient<IChatLieuService, ChatLieuService>();
-builder.Services.AddHttpClient();
 builder.Services.AddTransient<IServiceKieuDang, ServiceKieuDang>();
 var app = builder.Build();
 
@@ -46,46 +45,5 @@ app.UseEndpoints(endpoints =>
         pattern: "{area:exists}/{controller=HomeClient}/{action=Index}/{id?}",
         defaults: new { area = "Client", controller = "HomeClient", action = "Index" });
 
-});
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{area=Admin}/{controller=KieuDang}/{action=Getall}/{id?}");
-app.UseEndpoints(endpoints =>
-{
-    // Tuyến đường mặc định cho Admin
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{area=Admin}/{controller=DanhMuc}/{action=GetAll}/{id?}");
-
-    // Tuyến đường cho các area khác
-    endpoints.MapControllerRoute(
-        name: "areas",
-        pattern: "{area:exists}/{controller=DanhMuc}/{action=GetAll}/{id?}");
-
-    // Cấu hình route cho area Client
-    endpoints.MapControllerRoute(
-        name: "Client",
-        pattern: "{area:exists}/{controller=HomeClient}/{action=Index}/{id?}",
-        defaults: new { area = "Client", controller = "HomeClient", action = "Index" });
-
-	endpoints.MapControllerRoute(
-	  name: "thuonghieu-list",
-	  pattern: "Admin/ThuongHieu/Index",
-	  defaults: new { controller = "ThuongHieu", action = "Index" });
-    endpoints.MapControllerRoute(
-        name: "chatlieu-list",
-        pattern: "Admin/ChatLieu/Index",
-        defaults: new { controller = "ChatLieu", action = "Index" });
-
-    endpoints.MapControllerRoute(
-        name: "areas",
-        pattern: "{area:exists}/{controller=KieuDang}/{action=Getall}/{id?}");
-    endpoints.MapControllerRoute(
-        name: "Client",
-        pattern: "{area:exists}/{controller=HomeClient}/{action=Index}/{id?}",
-        defaults: new { area = "Client", controller = "HomeClient", action = "Index" });
-});
 });
 app.Run();
--- Domain/AppDbcontext.cs
+++ Domain/AppDbcontext.cs
@@ -68,7 +68,7 @@ namespace AppData
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=daantotnghiep;Trusted_Connection=True;TrustServerCertificate=True;");
+			optionsBuilder.UseSqlServer("Server=DESKTOP-UC1K64J\\SQLEXPRESS04;Database=daantotnghiep;Trusted_Connection=True;TrustServerCertificate=True;");
 		}
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
